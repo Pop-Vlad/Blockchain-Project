08aae247703eb8c5539f824f57133330cca0425e
Pop-Vlad <vladpop401@gmail.com>
2021-05-16 20:25:44 +0300

    finished project

--- README.txt
+++ README.txt
@@ -1,8 +1,30 @@
 
-IDEEA:
-Our idea is to create a lottery on the Ethereum blockchain. The contract allows the users to exchange ETH for game tokens implementing the ERC-20 standard.
-The users can use these tokens to take part in the lottery. A user can use 1 token for a chance to win and has a fixed probability of winning.
-The winner is decided based on a random number. When a user loses they receive nothing and their token is added to the prize pool.
-When a user wins they receive a fixed percentage of the prize pool. The rest is kept in the prize pool to avoid the value of the prize pool being too small.
-A user can exchange the tokens back for Eth, but with a fee. The owner can extract the accumulated fee by the contract.
-When the game starts or if the prize pool is too small, the owner can add tokens to the prize pool to incentivize users to play the game.
+See Presentation.pdf
+
+TEAM MEMBERS:
+
+Galatanu Tiberiu-Adrian
+Purcel Maria-Loredana
+Pop Vlad
+Hosu Dan-Marian
+
+
+IDEA:
+
+Our idea is to create a lottery on the Ethereum blockchain. The contract allows the users to exchange ETH for game tokens implementing the ERC-20 standard, called tickets. The users can use these tickets to take part in the lottery. A user can use 1 ticket for a chance to win and has a fixed probability of winning. The winner is decided based on generated random numbers. When a user loses they receive nothing and their token is added to the prize pool. When a user wins they receive a fixed percentage of the prize pool. The rest is kept in the prize pool to avoid the value of the prize pool being too small. When a user wins the amount of ETH corresponding to the accumulated prize pool is transferred to their account. The owner can extract the accumulated earnings from the contract fees. When the game starts, or if the prize pool is too small, the owner can add tokens to the prize pool to incentivize users to play the game and he can modify the percentage of tickets that are left in the prize pool when a user wins. The user can also view the prize pool, check it’s own amount of tickets, view the owner fee and transfer tickets to another user.
+
+
+DESCRIPTION:
+
+To buy tickets, a user calls “buyTickets(uint256 nrTickets)”, specifying the number of tickets they want to purchase. When calling the function they send a value greater or equal to the price of the tickets. The rest is returned to the user’s account.
+
+A user can try one ticket at a time by calling “tryTicket(uint8[] calldata numbers)”. This consumes 1 of their tickets. The user supplies an array of 5 numbers to the function representing the numbers guessed. If the numbers are correct, they win and a certain amount of ETH is transferred to their account. This is provided by the internal “win()” function and the amount won is: prizePool* (100-retainPrize)/100 * (100-exchangeFee)/100 * ticketPrice. Some of the prize is kept for the next winner (20%) and some is kept as fees for the owner (2%). The amount a user wins if they guess the numbers correctly is also given by the view function getPrize().
+
+A user can also see the ticket price getTicketPrice(), the fee: getExchangeFee() and the retained prize percentage for the next user: getRetainedPrize().
+
+The owner can withdraw the earnings of the contract’s fees with the withdrawEarnings() function. It returns the amount transferred to the owner. The amount the owner can withdraw is limited so it does not deplete the contract below the amount reserved for the winners:
+uint256 reservedBalance = _totalSupply * (100 - exchangeFee) / 100 * ticketPrice;
+
+The prize pool can be increased by boostPrize() function. It can be done from any account, but is normally done by the owner if the prize pool becomes too low, to incentivize users to use the lottery. The owner can also modify the fees (“updateFee(uint8 newFee)”) and retained prize pool (“updateRetainedPrize(uint8 newRetainedPrize)”), but only between some limits.
+
+The winning conditions: The numbers given by the user calling “tryTicket(uint8[] calldata numbers)” are compared to some random generated numbers, using the “equal(uint8[] memory a, uint8[] memory b, uint8 n)” internal function. This compares 2 arrays of length n to check if they contain the same numbers (in any order). The random numbers are generated using “generateRandomDistinctNumbers(uint8 _sampleSize, uint8 _domainSize)”. This generates _sampleSize distinct numbers in {1, 2, 3, ... , _domainSize}. This function uses “randMod(uint _modulus)” to generate 1 number at a time in the set {0, 1, …, _modulus}. To ensure the numbers are distinct and their distribution is uniform, each number is chosen as: “uint8 r = uint8(randMod(_domainSize-i)) + 1”, where i is the number of numbers already generated. Then r is increased by 1 for each generated number smaller than itself. Then r is added to the sorted list of generated numbers.
--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -11,19 +11,30 @@ contract Lottery is LotteryTokens, Ownable {
     uint8 retainPrize = 20;
     uint8 exchangeFee = 2;
     
-    
+    /**
+    * @dev Returns an integer value indicating the prize pool in ETH(80% of the `_totalSupply` with 2% `exchangeFee` by default).
+    */
     function getPrize() external view returns(uint256) {
         return _totalSupply * ticketPrice * (100 - retainPrize) / 100 * (100 - exchangeFee) / 100;
     }
     
+    /**
+    * @dev Returns an integer value indicating the current `exchangeFee`(2% by default).
+    */
     function getExchangeFee() external view returns(uint8) {
         return exchangeFee;
     }
     
+    /**
+    * @dev Returns an integer value indicating the current `retainPrize`(20% by default), which will be held for the next prize pool, after someone wins the current one.
+    */
     function getRetainedPrize() external view returns(uint8) {
         return retainPrize;
     }
     
+    /**
+    * @dev Returns an integer value indicating the current `ticketPrice`(0.001 ETH by default).
+    */
     function getTicketPrice() external view returns(uint256) {
         return ticketPrice;
     }
@@ -31,94 +42,99 @@ contract Lottery is LotteryTokens, Ownable {
     
     
     
+    /**
+    * @dev Adds a token to the balance of the function caller if the price was paid and moves the surplus back to the sender if they paid too much.
+    */
     function buyTickets(uint256 nrTickets) external payable {
+        // the value sent covers the price of the tickets
         require(msg.value >= ticketPrice * nrTickets);
         _balances[msg.sender] += nrTickets;
         uint256 rest = msg.value - ticketPrice * nrTickets;
         if (rest > 0){
+            // send the rest back
             msg.sender.transfer(rest);
         }
     }
     
     
     
-    function tryTicket(uint8[] calldata numbers) external {
+    
+    /**
+    * @dev Moves a token from the balance of the function caller and adds it to the `_totalSupply`.
+    *
+    * @dev Checks if the `numbers` are the same as the 5 randomly generated numbers.
+    *
+    * Calls function `equal()` to check if the 2 sets of numbers are the same.
+    *
+    * Calls win() if the numbers are the same as the ones generated.
+    */
+    function tryTicket(uint8[] calldata numbers) external returns(bool, uint8[] memory) {
         require(_balances[msg.sender] >= 1);
         _balances[msg.sender]--;
         _totalSupply++;
-        if (checkNumbers(numbers)){
+        
+        // generate the winning numbers
+        uint8[] memory winningNumbers = generateRandomDistinctNumbers(5, 50);
+        bool won = false;
+        if (equal(numbers, winningNumbers, 5)){
+            // the ticket won
+            won = true;
             win();
         }
+        return (won, winningNumbers);
     }
     
+    /**
+    * @dev Moves a percentage(80% by default) from the `_totalSupply` to the caller of the function, converted in ETH with a fee(2% by default).
+    */
     function win() internal {
+        // calculate won amount
         uint256 amountTokensWon = _totalSupply * (100 - retainPrize) / 100;
         _totalSupply -= amountTokensWon;
         uint amountEthToTransfer = amountTokensWon * (100 - exchangeFee) / 100 * ticketPrice;
+        // send prize to the user account
         msg.sender.transfer(amountEthToTransfer);
     }
     
-    function checkNumbers(uint8[] memory numbers) internal returns(bool) {
-        uint8[] memory winningNumbers = generateWinningNumbers();
-        return equal(numbers, winningNumbers, 5);
-    }
     
-    function generateWinningNumbers() internal returns(uint8[] memory) {
-        uint8[] memory generatedValues;
-        for (uint8 i=0; i<5;i++){
-            uint8 r = uint8(randMod(50-i));
+    /**
+    * @dev Generates an array with randomly generated values which are distinct and are sorted in ascending order.
+    * The number of generated numbers is _sampleSize. The numbers are picked from {1, 2, ..., _domainSize}
+    *
+    * Returns the generated array.
+    */
+    function generateRandomDistinctNumbers(uint8 _sampleSize, uint8 _domainSize) internal returns(uint8[] memory) {
+        uint8[] memory generatedValues = new uint8[](_sampleSize);
+        for (uint8 i=0; i<_sampleSize;i++){
+            
+            // generate a random number r
+            uint8 r = uint8(randMod(_domainSize-i)) + 1;
+            // increase r by 1 fo each value smaller than r.
             for(uint8 j=0; j<i; j++) {
                 if(generatedValues[j] < r){
                     r++;
                 }
             }
             
-            // keep array sorted
-            for(uint8 j=0; j<5; j++) {
-                if (r > generatedValues[j]) {
-                    continue;
-                }
-                else {
-                    uint8 aux = generatedValues[j];
-                    generatedValues[j] = r;
-                    r = aux;
-                    if (r == 0) {
-                        break;
-                    }
-                }
-            }
-        }
-        return generatedValues;
-    }
-    
-    function genTest() external returns(uint8[] memory) {
-        uint8[] memory generatedValues;
-        for (uint8 i=0; i<5;i++){
-            uint8 r = uint8(randMod(50-i));
+            // Add r to generatedValues
+            // keep array sorted after each addition
             for(uint8 j=0; j<i; j++) {
-                if(generatedValues[j] < r){
-                    r++;
-                }
-            }
-            
-            // keep array sorted
-            for(uint8 j=0; j<5; j++) {
-                if (r > generatedValues[j]) {
-                    continue;
-                }
-                else {
+                if (r < generatedValues[j]) {
                     uint8 aux = generatedValues[j];
                     generatedValues[j] = r;
                     r = aux;
-                    if (r == 0) {
-                        break;
-                    }
                 }
             }
+            generatedValues[i] = r;
         }
         return generatedValues;
     }
     
+    /**
+    * @dev Checks whether the arrays of numbers are the same.
+    *
+    * Returns a boolean value indicating whether the 2 given arrays are the same or not.
+    */
     function equal(uint8[] memory a, uint8[] memory b, uint8 n) internal pure returns(bool) {
         bool ok;
         for(uint8 i=0; i<n; i++) {
@@ -136,6 +152,9 @@ contract Lottery is LotteryTokens, Ownable {
         return true;
     }
     
+    /**
+    * @dev Returns a randomly generated integer.
+    */
     function randMod(uint _modulus) internal returns(uint) {
         randNonce++;
         return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
@@ -143,32 +162,57 @@ contract Lottery is LotteryTokens, Ownable {
     
     
     
-    function withdrawEarnings(uint256 _amount) external onlyOwner {
+    /**
+    * @dev Moves the earned amount of wei from the contract to the caller's account.
+    *
+    * Only the owner can withdraw and can't withdraw from the prize pool.
+    * Returns the amount transfered.
+    */
+    function withdrawEarnings() external onlyOwner returns(uint256) {
         uint256 reservedBalance = _totalSupply * (100 - exchangeFee) / 100 * ticketPrice;
-        require(_amount <= address(this).balance - reservedBalance);
+        uint256 _amount = address(this).balance - reservedBalance;
         address payable _owner = address(uint160(owner()));
         _owner.transfer(_amount);
+        return _amount;
     }
     
-    function boostPrize() external payable {
-        uint nrTickets = msg.value / ticketPrice;
+    /**
+    * @dev Adds tokens to the `__totalSupply` after the price was paid. The payer won't recieve anything in exchange.
+    *
+    * Returns an integer indicating the number of tokens that were added to the _totalSupply.
+    */
+    function boostPrize() external payable returns(uint256) {
+        uint256 nrTickets = msg.value / ticketPrice;
         _totalSupply += nrTickets;
         uint256 rest = msg.value - ticketPrice * nrTickets;
         if (rest > 0){
             msg.sender.transfer(rest);
         }
+        return nrTickets;
     }
     
     
     
-    function updateFee(uint8 newFee) external onlyOwner {
+    /**
+    * @dev Changes the current `exchangeFee` to the `newFee`.
+    *
+    * It can be called only by the owner.
+    */
+    function updateFee(uint8 newFee) external onlyOwner returns(uint8) {
         require(newFee <= 5);
         exchangeFee = newFee;
+        return exchangeFee;
     }
     
-    function updateRetainedPrize(uint8 newRetainedPrize) external onlyOwner {
+    /**
+    * @dev Changes the current `retainPrize` to the `newRetainedPrize`.
+    *
+    * It can be called only by the owner and can't be greater than 30 and less than 10.
+    */
+    function updateRetainedPrize(uint8 newRetainedPrize) external onlyOwner returns(uint8) {
         require(newRetainedPrize <= 30 &&  newRetainedPrize >= 10);
         retainPrize = newRetainedPrize;
+        return retainPrize;
     }
 }
 
